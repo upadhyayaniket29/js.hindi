333d81d492edd591484bfee62ed3b78462268c50
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2025-01-18 13:47:22 +0000

    Opeartions

--- js/conversionoperation.js
+++ js/conversionoperation.js
@@ -13,3 +13,13 @@ console.log(2+3+3+4)
 console.log(+true)
 console.log(+"")
 console.log(true)
+
+let y = 5;
+let x=5;
+
+let result = y++; // y is assigned to result first, then incremented
+console.log(y);      // Output: 6 (y is updated to 6)
+console.log(result);
+let result2=++x;
+console.log(x)
+console.log(result2)
