ef071b8912268d44bd900fa8fe27605f2d20d6d7
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2025-01-21 19:45:31 +0000

    strings

--- js/string.js
+++ js/string.js
@@ -1,4 +1,4 @@
-// const name="aniket"
+const name="aniket"
 // const repocount=10
 
 
@@ -9,9 +9,9 @@
 
 // console.log(`hello my name is  ${name} and my repo count is ${repocount}` )
 
-// const newname= new String('aniketu')
+const newname= new String('aniketu')
 
-// console.log(newname[0])
+console.log(newname[0])
 // console.log(newname.__proto__)
 
 // console.log(newname.length)
@@ -23,6 +23,9 @@
 
 
 // const newname2= newname.substring(0,4)
+// substring does not obey if we give its negative values it will start from 0 only
+// slice obey negative values as well , it can give negative values.
+
 // console.log(newname2)
 
 
@@ -31,30 +34,32 @@
 
 
 
-const name2=' aniket   '
+// const name2=' aniket   '
 // console.log(name2)
 // console.log(name2.trim())
+//  trim remove start and end space
 // // Trim is a function
 // console.log(name2.trimEnd())
 // console.log(name2.trimStart())
 
 // Replace and include
-const meraname='aniketUPA'
+// const meraname='aniketUPA'
 // console.log(meraname.replace('aniket','aryan'))
 // console.log(meraname.includes('gh'));
 
 // console.log(meraname.split(""))
+// split converts into array based on character 
 // console.log(meraname.toUpperCase());
 // console.log(meraname.toLowerCase());
 
 // console.log(meraname.valueOf())
 
 // ++++++++ toWellFormed ++++++++++
-console.log(meraname.toWellFormed())
+// console.log(meraname.toWellFormed())
 
 // +++++++++++ tostring ++++++++++++
 // Returns the value of string 
-console.log(meraname.toString())
+// console.log(meraname.toString())
 
 
 
