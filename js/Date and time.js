39d0b59153bdf2315ebb13041a419310571515ea
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2025-01-25 09:46:57 +0000

    Date and time

--- js/Date and time.js
+++ js/Date and time.js
@@ -1,16 +1,28 @@
 let mydate= new Date()
+console.log(mydate);
 console.log(mydate.toString())
 console.log(mydate.toDateString())
+console.log(mydate.toISOString());
+/*
+In JavaScript, ISO string refers to the ISO 8601 format, which is a standard for representing date and time. 
+It's commonly used to represent dates in a machine-readable format, ensuring consistency across different systems and time zones.
+
+*/
 console.log(mydate.toLocaleString())
 console.log(mydate.toLocaleTimeString())
 console.log(mydate.toJSON())
+// how to use JSON ?
+
+
 // console.log(mydate.toUTCString('en-IST'))
 console.log(typeof mydate)
 
-// Date is an Object 
+// ++++++++++Date is an Object ++++++++++ 
 
-let mycreatenewdate= new Date(2023 ,0,23)
+// let mycreatenewdate= new Date(2023 ,0,23)
+let mycreatenewdate= new Date("01-14-2023")
 console.log(mycreatenewdate.getTime())
+
 
 // in JS Month start with  0.
 // console.log(mycreatenewdate.toDateString())
@@ -29,7 +41,23 @@ console.log(newday.getMonth())
 console.log(newday.getYear())
 
 
-// newday.toLocaleString('default' ,{
-//     weekday:'long',
-// })
+/*newday.toLocaleString('default' ,{
+    weekday:'long',
+    timezone:
+})*/
+// ++++++++++++++++used in customisation of format +++++++++++++
+
+
 // This is defined as another new version
+let mytimestamp=Date.now()
+// 1970 se abhi tk ka milli sec value 
+
+console.log(mytimestamp)
+console.log(mycreatenewdate.getTime())
+
+// ++++++++++++++++++used to convert millisecond to second++++++++++++++++++++++++
+console.log(Math.floor(Date.now()/1000));
+
+// `${newDate.getDay()} and the time is `
+
+
