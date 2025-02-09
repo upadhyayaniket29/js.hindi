f059a20d174c9be3783dc04eafbe8923ff8c4a30
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2025-02-09 05:14:54 +0000

    Function2

--- js/function.js
+++ js/function.js
@@ -55,3 +55,34 @@ console.log(userloginmesssage("aniket")) //important point to keep mind
 
 //some are consider false in js like empty string"" is consider false and same undefined is also false
 
+function calculateCartPrice(val1,val2,...num1){ //we have used here rest operator , spread operator is also 3 dots same
+       return num1
+}
+console.log(calculateCartPrice(200,400,500,2000)) //isme ek hi retrun hoga kyoki function bnya for one only 
+// we will solve this by rest operator
+// A rest operator is a type of parameter that gets all of the remaining parameters of a function call via an Array.
+
+const user={
+    username:"hitesh",
+    price:199
+
+
+}
+function handleobjects(anyObjects){
+    console.log(`username is ${anyObjects.username} and price is ${anyObjects.price}`);
+    
+}
+// handleobjects(user)
+
+// can pass objects in below way also not required to define seprate 
+handleobjects({
+    username:"sam",
+    price:299
+})
+
+const newarray=[200,400,600,800]
+
+function returnSecondValue(getArray){
+ return getArray[1]
+}
+console.log(returnSecondValue(newarray))
