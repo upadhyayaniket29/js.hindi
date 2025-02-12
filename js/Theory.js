7188718f7eac5be761735673bb3b901fe5d9dd00
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2025-02-12 07:39:24 +0000

    Call Stack and Execution Context

---
+++ js/Theory.js
@@ -0,0 +1,24 @@
+// JS Execution Context
+
+// Global Execution Context
+// Function  Execution Context
+// Eval  Execution Context(Not Much imp) it is property of global execution context
+
+// Memory Creation phase or Creation Phase : isme hamne jo likha hai uske liye jagah allocate hoti hai all variables ko ikta krke rakha jata hai bs.
+
+// Execution Phase
+
+
+// 1. Global Execution is must it is first step inside the exceution of js it will be allocated in this 
+// 2.Memory Creattion Phase : Only Declaration of variable
+// 2.Execution Phase :- Exceution phase exceutes all variables and fucntions 
+//               Exceution again start hoga  if function comes in 
+/*
+The New Exceution that is created is deleted when work is done 
+*/
+
+
+// Call Stack 
+
+
+
